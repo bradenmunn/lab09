d4936c57a9e46aec8871308d38f4dbad74a7d85a
bradenmunn <bradenmunn@yahoo.com>
2019-04-02 01:09:38 -0500

    Added Todo tracker

---
+++ Todo.java
@@ -0,0 +1,26 @@
+public class Todo{
+/*
+ Tasks
+ 1. Complete the factorial method
+ 2. Complete the Fibonnaci method
+ 3. Complete the Tree class
+ 4. Complete the nnaryTree() method
+ 5. Complete the treeSum() method
+
+ Estimated Time
+ 1. 20
+ 2. 10
+ 3. 20
+ 4. 20
+ 5.
+
+ Actual Time
+ 1. 10
+ 2. 10
+ 3.20
+ 4.
+ 5.
+
+
+*/
+}
